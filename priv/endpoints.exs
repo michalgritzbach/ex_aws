aec6176757fa006abcb82cfbbb3ac6d974e43a44
Michal Gritzbach <gritzbach.michal@gmail.com>
2019-12-13 09:30:34 +0100

    Add eu-central-1 Rekognition endpoint

--- priv/endpoints.exs
+++ priv/endpoints.exs
@@ -1188,6 +1188,7 @@
           "endpoints" => %{
             "ap-northeast-1" => %{},
             "ap-southeast-2" => %{},
+            "eu-central-1" => %{},
             "eu-west-1" => %{},
             "us-east-1" => %{},
             "us-east-2" => %{},
